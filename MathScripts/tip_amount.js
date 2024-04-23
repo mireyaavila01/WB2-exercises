9e9b28f968430570d34a6799dbcacd114062f0d7
Mireya Avila <mireyaavila147@yahoo.com>
2024-04-23 10:04:03 -0400

    made tip amount

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var foodBill = 100;
+var tip =.20;
+
+var totalTip = foodBill * tip;
+
+console.log("The tip is " + totalTip);
+
+console.log("The tip on a " + foodBill + " food bill is " + totalTip);
+
